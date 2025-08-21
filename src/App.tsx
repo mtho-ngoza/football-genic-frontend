80e5811d8676b93a322cf27075fcddb6f7ca849b
mthob <mthobisimthembu15@gmail.com>
2025-08-21 22:28:07 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+import { CgAdidas } from 'react-icons/cg'
+import { SiNike } from 'react-icons/si'
+
+function App() {
+
+  return (
+    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white
+     text-2xl font-bold">
+    Hello world!<SiNike />.
+  </h1>
+  )
+}
+
+export default App

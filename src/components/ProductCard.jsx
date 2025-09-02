0d1bc0381c281bf2d54408800f367616c67466ca
mthob <mthobisimthembu15@gmail.com>
2025-09-03 00:49:42 +0200

    updated product view functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,34 +1,62 @@
+import ProductViewModal from "./ProductViewModal";
+import {useState} from "react";
+
 const ProductCard = ({
-    productId,
-    productName,
-    image,
-    description,
-    quantity,
-    price,
-    discount,
-    specialPrice
-}
-
-
-) => {
-    //const [openProductViewModal, setOpenProductViewModal] = useState(false);
-    const btnHnadler = false;
-    //const [selectedViewProduct, setSelectedViewProduct] = useState();
+                         productId,
+                         productName,
+                         image,
+                         description,
+                         quantity,
+                         price,
+                         discount,
+                         specialPrice
+                     }) => {
+    const [openProductViewModal, setOpenProductViewModal] = useState(false);
+    const btnLoader = false;
+    const [selectedViewProduct, setSelectedViewProduct] = useState("");
     const isAvailable = quantity && Number(quantity) > 0;
 
-
+    const handleProductView = (product) => {
+        setSelectedViewProduct(product);
+        setOpenProductViewModal(true);
+    };
 
     return (
-        <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300" >
-            <div onClick={() => { }} className="w-full overflow-hidden aspect-[3/2]">
-                <img className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
+        <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
+            <div onClick={() => {
+                handleProductView({
+                    id: productId,
+                    productName,
+                    image,
+                    description,
+                    quantity,
+                    price,
+                    discount,
+                    specialPrice,
+                })
+            }}
+                 className="w-full overflow-hidden aspect-[3/2]">
+                <img
+                    className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
                     src={image}
                     alt={productName}>
                 </img>
             </div>
             <div className="p-4">
                 <h2
-                    onClick={() => { }}
+                    onClick={() => {
+                        handleProductView({
+                            id: productId,
+                            productName,
+                            image,
+                            description,
+                            quantity,
+                            price,
+                            discount,
+                            specialPrice,
+                        })
+                    }}
+
                     className="text-lg font-semibold mb-2 cursor-pointer">
                     {productName}
                 </h2>
@@ -37,20 +65,19 @@ const ProductCard = ({
                     <p className="text-gray-600 text-sm">
                         {description}
                     </p>
-
                 </div>
 
-                <div className="flex items-center justify">
+                <div className="flex items-center justify-between">
                     {specialPrice ? (
-                        <div className="flex flex-col">
+                            <div className="flex flex-col">
                             <span className="text-gray-400 line-through">
                                 R{Number(price).toFixed(2)}
                             </span>
-                            <span className="text-xl font-bold text-red-600">
+                                <span className="text-xl font-bold text-red-600">
                                 R{Number(specialPrice).toFixed(2)}
                             </span>
-                        </div>
-                    ) :
+                            </div>
+                        ) :
                         (
                             <div>
                                 <span className="text-xl font-bold text-slate-700">
@@ -60,15 +87,23 @@ const ProductCard = ({
                             </div>
                         )}
 
-                    <button className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}`}
-                    text-white py-2 px-3 rounded-lg items-center transition duration-300 ml-auto
-                    >
+                    <button
+                        className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
+                        text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
                         {isAvailable ? "Add to Cart" : "Out of Stock"}
                     </button>
 
                 </div>
 
+
             </div>
+            <ProductViewModal
+                open={openProductViewModal}
+                setOpen={setOpenProductViewModal}
+                product={selectedViewProduct}
+                isAvailable={isAvailable}
+
+            />
         </div>
     )
 }
